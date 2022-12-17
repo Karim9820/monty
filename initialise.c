847ba7f0d0b61f3c382f6eb831aeb8cd7c1be505
Abdul Juma <karimjuma9820@gmail.com>
2022-12-17 17:46:14 +0000

    monty

---
+++ initialise.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+/**
+* execute - executes the opcode
+* @stack: head linked list - stack
+* @counter: line_counter
+* @file: poiner to monty file
+* @content: line content
+* Return: no return
+*/
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", __push}, {"pall", __pall}, {"pint", __pint},
+				{"pop", __pop},
+				{"swap", __swap},
+				{"add", __add},
+				{"nop", __nop},
+				{"sub", __sub},
+				{"div", __div},
+				{"mul", __mul},
+				{"mod", __mod},
+				{"pchar", __pchar},
+				{"pstr", __pstr},
+				{"rotl", __rotl},
+				{"rotr", __rotr},
+				{"queue", __queue},
+				{"stack", __stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
